f6b9d674b0ba02bd3d4d5f0c618d460d3ba1330b
Luan de Oliveira <luan012012@hotmail.com>
2024-09-08 22:38:44 -0300

    feat: criado projeto

---
+++ src/main/java/com/luandeoliveira/ccb_api/CcbApiApplication.java
@@ -0,0 +1,13 @@
+package com.luandeoliveira.ccb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcbApiApplication.class, args);
+	}
+
+}
