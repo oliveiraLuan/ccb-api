8d7fb283d3ed3968821566046ef5560ba6ed3ebf
Luan de Oliveira <luan012012@hotmail.com>
2024-09-09 21:40:31 -0300

    feat: criado HymnException

---
+++ src/main/java/com/luandeoliveira/ccb_api/services/exceptions/HymnException.java
@@ -0,0 +1,7 @@
+package com.luandeoliveira.ccb_api.services.exceptions;
+
+public class HymnException extends RuntimeException{
+    public HymnException(String message){
+        super(message);
+    }
+}
